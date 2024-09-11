9153140ea091ec49c2375dade544b03a46bbe43a
hadooni <hayoung7259@gmail.com>
2024-09-11 12:38:57 +0900

    advanced

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,49 @@ const App = () => {
   const [views, setViews] = useState("");
   const queryClient = useQueryClient();
 
+  const API_URL = "http://localhost:3000";
+
   const getPosts = async () => {
-    const response = await axios.get("http://localhost:3000/posts");
+    const response = await axios.get(`${API_URL}/posts`);
+    return response.data;
+  };
+
+  const getProfile = async () => {
+    const response = await axios.get(`${API_URL}/profile`);
+    return response.data;
+  };
+
+  const getComment = async () => {
+    const response = await axios.get(`${API_URL}/comments`);
     return response.data;
   };
 
   const setPost = async (newPost) => {
-    await axios.post("http://localhost:3000/posts", {
+    await axios.post(`${API_URL}/posts`, {
       title: newPost.title,
       views: newPost.views,
     });
   };
 
-  const { data, isLoading, isError } = useQuery({
+  const {
+    data: posts,
+    isLoading: postLoading,
+    isError: postError,
+  } = useQuery({
     queryKey: ["posts"],
     queryFn: getPosts,
   });
+
+  const { isLoading: profileLoading, isError: profileError } = useQuery({
+    queryKey: ["profile"],
+    queryFn: getProfile,
+  });
+
+  const { data: comments, refetch } = useQuery({
+    queryKey: ["comments", "id"],
+    queryFn: getComment,
+  });
+  console.log("comments :>> ", comments);
 
   const mutation = useMutation({
     mutationFn: setPost,
@@ -31,12 +58,20 @@ const App = () => {
     },
   });
 
-  if (isLoading) {
+  if (postLoading) {
     return <div>로딩중입니다...</div>;
   }
 
-  if (isError) {
+  if (postError) {
     return <div>오류가 발생하였습니다...</div>;
+  }
+
+  if (profileLoading) {
+    return <div>프로필 로딩중입니다...</div>;
+  }
+
+  if (profileError) {
+    return <div>오류가 발생했습니다...</div>;
   }
 
   return (
@@ -59,11 +94,20 @@ const App = () => {
         입력
       </button>
 
-      {data.map((post) => {
+      {posts.map((post) => {
         return (
           <div key={[post.id]}>
             <p>
               {post.title} (views : {post.views})
+              <button onClick={() => refetch()}>댓글보기</button>
+              {/* {comments.map((comment) => {
+                return (
+                  <div key={comment.id}>
+                    <p>작성자 아이디 : {comment.postId}</p>
+                    <p>{comment.text}</p>
+                  </div>
+                );
+              })} */}
             </p>
           </div>
         );
